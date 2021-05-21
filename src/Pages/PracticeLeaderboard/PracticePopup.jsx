cfcd6c90b9500a756064a58a30a150df53766efc
SoftwareFuze <atticus@softwarefuze.com>
2021-05-21 06:45:11 -0700

    Mobile responsiveness [0]

--- src/Pages/PracticeLeaderboard/PracticePopup.jsx
+++ src/Pages/PracticeLeaderboard/PracticePopup.jsx
@@ -5,7 +5,7 @@ export default function PracticePopup(props) {
     const [ isValidTime, setIsValidTime ] = useState(true);
 
     useEffect(() => {
-        if (parseInt(minutes) > 300) {
+        if (parseInt(minutes) > 400) {
             setIsValidTime(false);
         }
     }, [ minutes ]);
@@ -13,7 +13,7 @@ export default function PracticePopup(props) {
     return (
         <div className="practice-time-popup" style={{ display: props.visible ? "flex" : "none" }}>
             <h1>Practice Time {(minutes !== 0 && Math.round(minutes) > 29) && `— ${minutes} min`}</h1>
-            <input type="number" placeholder="Practice time in minutes..." onChange={e => setMinutes(e.target.value)} required />
+            <input type="number" placeholder="Practice time in minutes..." onChange={e => parseInt(e.target.value) > 29 && setMinutes(e.target.value)} required />
             <input placeholder="Instrument..." required />
             {!isValidTime && <p>You couldn't have practiced that long...</p>}
             {(minutes !== 0 && minutes < 30) && <p>That isn't a qualifying time...</p>}
